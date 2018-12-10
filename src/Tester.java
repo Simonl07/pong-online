71de8ed35f60cf7972e3c8a930e8f009175033d3
Simon <simon980425@126.com>
2018-12-10 13:54:05 -0800

    a

--- src/Tester.java
+++ src/Tester.java
@@ -3,28 +3,9 @@ import client.util.AverageRateOfChangeQueue;
 public class Tester {
 
 	public static void main(String args[]){
-		
-		
-		AverageRateOfChangeQueue<Integer> a = new AverageRateOfChangeQueue<>(3);
-		
-		a.add(3);
-		try {
-			Thread.sleep(20);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	
+		for(int i = 0; i < 1000000000; i++){
+			System.out.println(System.currentTimeMillis());
 		}
-		a.add(5);
-		try {
-			Thread.sleep(20);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}		
-		a.add(6);
-		System.out.println(a);
-		
-		System.out.println(a.getValueROC());	
-		System.out.println(a.getEntryFrequency(10));
 	}
 }
