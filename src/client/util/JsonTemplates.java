f7467c0230e6946cd3767c74f14972efcc1ffb91
Simon <simon980425@126.com>
2018-12-11 16:58:35 -0800

    sate

--- src/client/util/JsonTemplates.java
+++ src/client/util/JsonTemplates.java
@@ -19,7 +19,7 @@ public final class JsonTemplates {
 
 	public static JsonObject IG_CLIENT_REFLECT(int x, int y, double dx, double dy, long start) {
 		JsonObject json = new JsonObject();
-		json.addProperty("type", "mm_client_cancel");
+		json.addProperty("type", "ig_client_reflect");
 		JsonObject v = new JsonObject();
 		v.addProperty("x", x);
 		v.addProperty("y", y);
